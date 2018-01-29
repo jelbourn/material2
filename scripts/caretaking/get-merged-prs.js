03edfd815307de97c051b1a464647076dd6de25b
Jeremy Elbourn <jelbourn@google.com>
2018-01-29 13:26:09 -0800

    scipt

---
+++ scripts/caretaking/get-merged-prs.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const GitHubApi = require('github');
+const github = new GitHubApi();
+
+
+github.search.issues({
+  per_page: 100,
+  q: 'repo:angular/material2 is:pr is:merged merged:>2018-01-22 -label:"release: minor"',
+}, (error, response) => {
+  if (response) {
+
+    const prs = response.data.items;
+    for (const pr of prs) {
+      console.log(`${pr.number}`);
+    }
+  } else {
+    console.error('Fetching merged PRs failed.');
+    console.error(error);
+  }
+});
