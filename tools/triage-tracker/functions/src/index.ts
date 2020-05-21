b4056c358df11994c8ce4c0c5d9e1bbbcf48fc44
Jeremy Elbourn <jelbourn@google.com>
2020-05-21 16:00:50 -0700

    Validate message is coming from Github, log request body

--- tools/triage-tracker/functions/src/index.ts
+++ tools/triage-tracker/functions/src/index.ts
@@ -1,8 +1,27 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as crypto from 'crypto';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const logGitHubEvent = functions.https.onRequest(async (req, response) => {
+  // GitHub webhook has a secret associated with it. We have the same secret in the Firebase
+  // function config. We sha1 hash that secret from config and compare it to what GitHub sent.
+  const signatureFromGithub = req.headers['x-hub-signature'];
+  const hmac = crypto.createHmac('sha1', functions.config().github.secret)
+      .update(req.rawBody)
+      .digest('hex');
+  const expectedSignature = `sha1=${hmac}`;
+
+  if (signatureFromGithub !== expectedSignature) {
+    console.error('x-hub-signature', signatureFromGithub, 'did not match', expectedSignature);
+    return response.status(403).send('x-hub-signature does not match expected signature');
+  }
+
+  // const event = request.body;
+  // const snapshot = await admin.database().ref('/messages').push({original: request});
+  console.log(JSON.stringify(req.body));
+
+  return response.status(200).send();
+});
+
